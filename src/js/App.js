f6c85185452f3077844bb261fcb6a4e551a49e78
Evan Winter <ewinter@sccadv.com>
2019-12-06 10:58:25 -0600

    update parcel version

--- src/js/App.js
+++ src/js/App.js
@@ -5,12 +5,15 @@ const App = () => {
 	const [data, setData] = useState(null)
 
 	const fetchData = async () => {
-		console.log('Fetching data...')
 
-		const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
-		const json = await response.json()
+		if (!data) {
+			console.log('Fetching data...')
 
-		if (json) setData(json)
+			const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+			const json = await response.json()
+
+			if (json) setData(json)
+		}
 	}
 
 	return (
