9c81600c7f3000a76a2b5372accae7905616ff48
Evan Winter <ewinter@sccadv.com>
2020-01-22 10:20:31 -0600

    init

--- src/js/App.js
+++ src/js/App.js
@@ -1,27 +1,103 @@
 import React, { useState } from "react"
 
-const App = () => {
+export const Form = () => {
+	const [query, setQuery] = useState("")
+	const [mode, setMode] = useState("encrypt")
+	const [result, setResult] = useState([])
 
-	const [data, setData] = useState(null)
+	const handleTextChange = (e) => setQuery(e.target.value)
+	const handleRadioChange = (e) => setMode(e.target.value)
 
-	const fetchData = async () => {
-
-		if (!data) {
-			console.log('Fetching data...')
-
-			const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
-			const json = await response.json()
-
-			if (json) setData(json)
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		if (query && query.trim()) {
+			const result = await displayResults(query, mode)
+			setResult(result)
 		}
 	}
 
-	return (
-		<div className="App" onClick={fetchData}>
-			Hello World
+	const encrypt = async (data) => {
+		const encryptURL = "https://forms.stormcloud.ninja/dev/general_encrypt/"
+		const url = encryptURL + data
+		const result = await fetch(url)
+		const html = await result.text()
+		return html
+	}
 
-			<div>{data ? JSON.stringify(data) : ''}</div>
+	const decrypt = async (data) => {
+		const decryptURL = "https://forms.stormcloud.ninja/dev/general_decrypt/"
+		const url = decryptURL + data
+		const result = await fetch(url)
+		const html = await result.text()
+		return html
+	}
+
+	const displayResults = async (query, mode) => {
+		return mode === "encrypt" ? await encrypt(query) : await decrypt(query)
+	}
+
+	return (
+		<>
+			<FormInput
+				handleRadioChange={handleRadioChange}
+				handleTextChange={handleTextChange}
+				handleSubmit={handleSubmit}
+			/>
+			<FormResults result={result} />
+		</>
+	)
+}
+
+export const FormInput = ({
+	handleRadioChange,
+	handleTextChange,
+	handleSubmit,
+}) => {
+	return (
+		<div className="FormInput">
+			<form onSubmit={handleSubmit}>
+				<div className="input-group">
+					<label htmlFor="encrypt">Encrypt</label>
+					<input
+						onChange={handleRadioChange}
+						defaultChecked
+						id="encrypt"
+						type="radio"
+						name="encrypt-decrypt"
+						value="encrypt"
+					/>
+				</div>
+				<div className="input-group">
+					<label htmlFor="decrypt">Decrypt</label>
+					<input
+						onChange={handleRadioChange}
+						id="decrypt"
+						type="radio"
+						name="encrypt-decrypt"
+						value="decrypt"
+					/>
+				</div>
+				<div className="input-group">
+					<input onChange={handleTextChange} type="text" />
+					<button className="button">Submit</button>
+				</div>
+			</form>
 		</div>
 	)
 }
+
+export const FormResults = ({ result }) => {
+	const parser = new DOMParser()
+	const html = parser.parseFromString(result, "text/html")
+	const containerElement = html.querySelector("p")
+	const value = containerElement ? containerElement.innerText : ""
+	return <div className="FormResults">{value}</div>
+}
+
+const App = () => (
+	<div className="App container">
+		<Form />
+	</div>
+)
+
 export default App
